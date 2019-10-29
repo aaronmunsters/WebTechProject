15f3570063fc31d22bbd331b0f542e015bd739f1
wulfdewolf <wolf2wulf@gmail.com>
2019-10-29 13:49:28 +0100

    Added validation of register/login input via joi, this is done by using @hapi.joi package, validation is done by functions in validation.js file

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,32 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const {registerValidation, loginValidation} = require('../validation');
+
 
 // Register route
 router.post('/register', async (req, res) => {
+
+    // Validate date before making new user
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
     const user = new User({
         name:       req.body.name,
         email:      req.body.email,
         password:   req.body.password
     });
+    
     try{
         console.log("got here"); 
-        const savedUser = await user.save();
-        console.log("but not here");
+        let savedUser = await user.save();
+        console.log(savedUser);
         res.send(savedUser)
     } catch(err){
         res.status(400).send(err);
     }
 });
 
-// ---> localhost:3000:/api/user/register
+// ---> localhost:3000/api/user/register
 
 
 // Login route
@@ -26,7 +34,7 @@ router.post('/login', (req, res) => {
     res.send('Login');
 });
 
-// ---> localhost:3000:/api/user/login
+// ---> localhost:3000/api/user/login
 
 // Export the router such that it can be used in index.js
 module.exports = router;
