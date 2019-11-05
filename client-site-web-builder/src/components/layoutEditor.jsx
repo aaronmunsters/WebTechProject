bb8cd3902d53695de1f640d3458aabe00971a6cf
FastFoodChicken <aaronmunstersbxl@gmail.com>
2019-11-05 15:42:50 +0100

    Added basic layout-edit options

--- client-site-web-builder/src/components/layoutEditor.jsx
+++ client-site-web-builder/src/components/layoutEditor.jsx
@@ -1,9 +1,124 @@
 import React, { Component } from "react";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 
 class LayoutEditor extends Component {
-  state = {};
+  standards = {
+    collTypes: [
+      { id: "single", look: "███████" },
+      { id: "small-left", look: "██ ████" },
+      { id: "small-right", look: "████ ██" },
+      { id: "triple", look: "█ ███ █" }
+    ],
+
+    backgroundColors: ["white", "black", "dark-blue", "dark-green"],
+    navigationBars: ["none", "simple"]
+  };
+
+  state = {
+    navigationBar: false,
+    collType: this.standards.collTypes[0],
+    backgroundColor: this.standards.backgroundColors[0],
+    navigationBar: this.standards.navigationBars[1]
+  };
+
+  generateButtons = () => {
+    let onClickF = idx => {
+      return () => {
+        this.setState({ columns: idx });
+      };
+    };
+
+    function giveButton(idx) {
+      return <Button onClick={onClickF(idx)}>{idx}</Button>;
+    }
+
+    return Array(this.standards.maxColls)
+      .fill(null)
+      .map((v, idx) => giveButton(idx + 1));
+  };
+
   render() {
-    return <h3>WORK IN PROGRESS</h3>;
+    return (
+      <Col>
+        <Row>
+          <h4>Edit the site below, finally hit save!</h4>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Choose the your type of columns</h2>
+          </Col>
+          <Col>
+            <DropdownButton
+              as={ButtonGroup}
+              title={this.state.collType.look}
+              id="bg-nested-dropdown"
+            >
+              {this.standards.collTypes.map(collT => (
+                <Dropdown.Item
+                  key={collT.id}
+                  eventKey={collT.id}
+                  onClick={() => this.setState({ collType: collT })}
+                >
+                  {collT.look}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Choose your backgroundcolor</h2>
+          </Col>
+          <Col>
+            <DropdownButton
+              as={ButtonGroup}
+              title={this.state.backgroundColor}
+              id="bg-nested-dropdown"
+            >
+              {this.standards.backgroundColors.map(bgC => (
+                <Dropdown.Item
+                  key={bgC}
+                  eventKey={bgC}
+                  onClick={() => this.setState({ backgroundColor: bgC })}
+                >
+                  {bgC}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Choose your navigation bar</h2>
+          </Col>
+          <Col>
+            <DropdownButton
+              as={ButtonGroup}
+              title={this.state.navigationBar}
+              id="bg-nested-dropdown"
+            >
+              {this.standards.navigationBars.map(nbT => (
+                <Dropdown.Item
+                  key={nbT}
+                  eventKey={nbT}
+                  onClick={() => this.setState({ navigationBar: nbT })}
+                >
+                  {nbT}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+          </Col>
+        </Row>
+        <Row>
+          <Button variant="success">SAVE</Button>
+        </Row>
+      </Col>
+    );
   }
 }
 
