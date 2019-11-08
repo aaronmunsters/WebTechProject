14feb0bac0056e6c0ba1657b6c183eef47d5ea3b
FastFoodChicken <aaronmunstersbxl@gmail.com>
2019-11-08 15:15:50 +0100

    splitses dropdown menus in different class

--- client-site-web-builder/src/components/layoutEditor.jsx
+++ client-site-web-builder/src/components/layoutEditor.jsx
@@ -7,22 +7,79 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import axios from "axios";
 
+class DropdownSelection extends Component {
+  /* props required are: selectedIdx, choices: [{id, callback, title}] */
+  state = { selected: this.props.choices[this.props.selectedIdx] };
+  render() {
+    return (
+      <DropdownButton title={this.state.selected.title}>
+        {this.props.choices.map(choice => (
+          <Dropdown.Item
+            key={choice.id}
+            onClick={() => {
+              this.setState({ selected: choice });
+              if (this.props.generalCallback) {
+                this.props.generalCallback(choice);
+              } else {
+                choice.callback(choice);
+              }
+            }}
+          >
+            {choice.title}
+          </Dropdown.Item>
+        ))}
+      </DropdownButton>
+    );
+  }
+}
+
 class LayoutEditor extends Component {
   standards = {
-    collTypes: [
-      { id: "single", look: "███████" },
-      { id: "small-left", look: "██ ████" },
-      { id: "small-right", look: "████ ██" },
-      { id: "triple", look: "█ ███ █" }
+    collTypeCallback: choice => {
+      let data = this.state.data;
+      data.collType = choice.id;
+      this.setState({ data: data });
+    },
+
+    collTypesCorrect: [
+      { id: "single", title: "███████" },
+      { id: "small-left", title: "██ ████" },
+      { id: "small-right", title: "████ ██" },
+      { id: "triple", title: "█ ███ █" }
     ],
 
-    backgroundColors: ["white", "black", "dark-blue", "dark-green"],
-    navigationBars: ["none", "simple"]
+    colorCallback: choice => {
+      let data = this.state.data;
+      data.backgroundColor = choice.id;
+      this.setState({ data: data });
+    },
+
+    backgroundColors: [
+      { id: "white", title: "white" },
+      { id: "black", title: "black" },
+      { id: "dark-blue", title: "dark-blue" },
+      { id: "dark-green", title: "dark-green" }
+    ],
+
+    navbarCallback: choice => {
+      let data = this.state.data;
+      data.navigationBar = choice.id;
+      this.setState({ data: data });
+    },
+
+    navigationBars: [
+      { id: "none", title: "none" },
+      { id: "simple", title: "simple" }
+    ]
   };
 
   state = {
     serverFetched: false,
-    data: {}
+    data: {
+      collType: "uninitialised",
+      backgroundColor: "uninitialised",
+      navigationBar: "uninisialised"
+    }
   };
 
   componentDidMount = async () => {
@@ -46,25 +103,13 @@ class LayoutEditor extends Component {
             <h2>Choose the your type of columns</h2>
           </Col>
           <Col>
-            <DropdownButton
-              as={ButtonGroup}
-              title={this.state.data.collType.look}
-              id="bg-nested-dropdown"
-            >
-              {this.standards.collTypes.map(collT => (
-                <Dropdown.Item
-                  key={collT.id}
-                  eventKey={collT.id}
-                  onClick={() => {
-                    let data = this.state.data;
-                    data.collType = collT;
-                    this.setState({ data: data });
-                  }}
-                >
-                  {collT.look}
-                </Dropdown.Item>
-              ))}
-            </DropdownButton>
+            <DropdownSelection
+              selectedIdx={this.standards.collTypesCorrect.findIndex(
+                choice => choice.id === this.state.data.collType
+              )}
+              choices={this.standards.collTypesCorrect}
+              generalCallback={this.standards.collTypeCallback}
+            />
           </Col>
         </Row>
         <Row>
@@ -72,25 +117,13 @@ class LayoutEditor extends Component {
             <h2>Choose your backgroundcolor</h2>
           </Col>
           <Col>
-            <DropdownButton
-              as={ButtonGroup}
-              title={this.state.data.backgroundColor}
-              id="bg-nested-dropdown"
-            >
-              {this.standards.backgroundColors.map(bgC => (
-                <Dropdown.Item
-                  key={bgC}
-                  eventKey={bgC}
-                  onClick={() => {
-                    let data = this.state.data;
-                    data.backgroundColor = bgC;
-                    this.setState({ data: data });
-                  }}
-                >
-                  {bgC}
-                </Dropdown.Item>
-              ))}
-            </DropdownButton>
+            <DropdownSelection
+              selectedIdx={this.standards.backgroundColors.findIndex(
+                choice => choice.id === this.state.data.backgroundColor
+              )}
+              choices={this.standards.backgroundColors}
+              generalCallback={this.standards.colorCallback}
+            />
           </Col>
         </Row>
         <Row>
@@ -98,25 +131,13 @@ class LayoutEditor extends Component {
             <h2>Choose your navigation bar</h2>
           </Col>
           <Col>
-            <DropdownButton
-              as={ButtonGroup}
-              title={this.state.data.navigationBar}
-              id="bg-nested-dropdown"
-            >
-              {this.standards.navigationBars.map(nbT => (
-                <Dropdown.Item
-                  key={nbT}
-                  eventKey={nbT}
-                  onClick={() => {
-                    let data = this.state.data;
-                    data.navigationBar = nbT;
-                    this.setState({ data: data });
-                  }}
-                >
-                  {nbT}
-                </Dropdown.Item>
-              ))}
-            </DropdownButton>
+            <DropdownSelection
+              selectedIdx={this.standards.navigationBars.findIndex(
+                choice => choice.id === this.state.data.navigationBar
+              )}
+              choices={this.standards.navigationBars}
+              generalCallback={this.standards.navbarCallback}
+            />
           </Col>
         </Row>
         <Row>
