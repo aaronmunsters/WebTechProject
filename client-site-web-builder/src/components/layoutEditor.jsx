15e267882b27d8c13abd69cf18f8472b1f89d80c
FastFoodChicken <aaronmunstersbxl@gmail.com>
2019-11-08 17:15:16 +0100

    Fixed page not loading

--- client-site-web-builder/src/components/layoutEditor.jsx
+++ client-site-web-builder/src/components/layoutEditor.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
