e1b15b9c76d400b688bca10932e9c80a98334ed0
wulfdewolf <wolf2wulf@gmail.com>
2019-11-19 14:18:58 +0100

    readded the testDB.sql file to the database folder since it was missing

---
+++ database/testDB.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: WoxDB
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.19.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Components`
+--
+
+DROP TABLE IF EXISTS `Components`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Components` (
+  `componentId` varchar(20) NOT NULL,
+  `creatorName` varchar(20) DEFAULT NULL,
+  `content` json DEFAULT NULL,
+  `pages` json DEFAULT NULL,
+  `date` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`componentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Components`
+--
+
+LOCK TABLES `Components` WRITE;
+/*!40000 ALTER TABLE `Components` DISABLE KEYS */;
+INSERT INTO `Components` VALUES ('testComp','Wolf','{}','{}',NULL);
+/*!40000 ALTER TABLE `Components` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Layouts`
+--
+
+DROP TABLE IF EXISTS `Layouts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Layouts` (
+  `layoutId` varchar(20) NOT NULL,
+  `backgroundColor` varchar(20) DEFAULT NULL,
+  `coltype` varchar(20) DEFAULT NULL,
+  `navBar` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`layoutId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Layouts`
+--
+
+LOCK TABLES `Layouts` WRITE;
+/*!40000 ALTER TABLE `Layouts` DISABLE KEYS */;
+INSERT INTO `Layouts` VALUES ('testLayout','\"black\"','\"double\"','\"simple\"');
+/*!40000 ALTER TABLE `Layouts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Pages`
+--
+
+DROP TABLE IF EXISTS `Pages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Pages` (
+  `pageId` varchar(20) NOT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `creatorName` varchar(20) DEFAULT NULL,
+  `published` varchar(45) DEFAULT NULL,
+  `comps` json DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`pageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Pages`
+--
+
+LOCK TABLES `Pages` WRITE;
+/*!40000 ALTER TABLE `Pages` DISABLE KEYS */;
+INSERT INTO `Pages` VALUES ('testPage','This is a test','Wolf','0','{}',NULL);
+/*!40000 ALTER TABLE `Pages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(1024) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES ('Wolf','wolf@vub.be','test','admin',NULL);
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-19 14:17:44
