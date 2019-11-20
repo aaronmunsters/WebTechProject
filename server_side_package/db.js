c46c162155cd7286dcfad625cbe0337342a470b8
wulfdewolf <wolf2wulf@gmail.com>
2019-11-20 17:20:06 +0100

    changed the dblogin to the old stuff untill the docker-compose works, don't forget to create your .env file with DB_PASS=<password> to be able to run

--- server_side_package/db.js
+++ server_side_package/db.js
@@ -6,10 +6,10 @@ const dotenv = require('dotenv');
 
 // Local mysql db connection
 var connection = mysql.createConnection({
-    host     : process.env.DATABASE_HOST || '0.0.0.0',
+    host     : 'localhost',
     port     : '3306',
     user     : 'root',
-    password : process.env.DATABASE_PASSWORD,
+    password : process.env.DB_PASS,
     database : 'WoxDB',
     dateStrings: 'date'
 });
