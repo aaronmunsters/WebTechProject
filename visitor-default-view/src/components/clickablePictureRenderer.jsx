81ac2238b4937be7ef03ffd019fd84712a7c8da0
FastFoodChicken <aaronmunstersbxl@gmail.com>
2019-12-10 15:52:47 +0100

    Fixed radius warning

--- visitor-default-view/src/components/clickablePictureRenderer.jsx
+++ visitor-default-view/src/components/clickablePictureRenderer.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 const CliclablePictureRenderer = props => {
   const { content } = props;
   return (
     <a href={content.link}>
       <img
-        style={{ "border-radius": "50%" }}
+        style={{ borderRadius: "50%" }}
         src={content.online ? content.source : content.id}
         alt={content.alt} // should get fetched from the database
       ></img>
