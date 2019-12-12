911c74b7dc565228bc569e83b8d999c48b27a8af
FastFoodChicken <aaronmunstersbxl@gmail.com>
2019-12-12 16:10:18 +0100

    Cleaned single column case

--- visitor-default-view/src/components/columnsRenderer.jsx
+++ visitor-default-view/src/components/columnsRenderer.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Tab, Tabs } from "react-bootstrap";
 import CompColRenderer from "./compColRenderer";
+import { complementColor } from "./generalFunctions";
+
 class ColumnsRenderer extends Component {
   state = {};
 
@@ -40,27 +42,67 @@ class ColumnsRenderer extends Component {
 
   render() {
     const { leftSize, middleSize, rightSize } = this.state;
-    const { compsL, compsM, compsR } = this.props;
-    const style = this.props.layout;
-    return (
-      <Container fluid={true} style={{ padding: "1rem" }}>
-        <Row>
-          {leftSize ? (
-            <Col sm={leftSize}>
-              <CompColRenderer style={style} ids={compsL} />
-            </Col>
-          ) : null}
-          <Col sm={middleSize}>
-            <CompColRenderer style={style} ids={compsM} />
-          </Col>
-          {rightSize ? (
-            <Col sm={rightSize}>
-              <CompColRenderer style={style} ids={compsR} />
-            </Col>
-          ) : null}
-        </Row>
-      </Container>
+    const { compsL, compsM, compsR, layout } = this.props;
+    const { followstyle, backgroundColor } = layout;
+
+    let bgc = backgroundColor;
+    if (followstyle) bgc = complementColor(bgc);
+    const collsStyle = { ...layout, backgroundColor: bgc, margin: "1%" };
+
+    const leftCol = leftSize ? (
+      <Col md={leftSize}>
+        <CompColRenderer style={collsStyle} ids={compsL} />
+      </Col>
+    ) : null;
+
+    const middleCol = (
+      <Col md={middleSize}>
+        <CompColRenderer style={collsStyle} ids={compsM} />
+      </Col>
     );
+
+    const rightCol = rightSize ? (
+      <Col md={rightSize}>
+        <CompColRenderer style={collsStyle} ids={compsR} />
+      </Col>
+    ) : null;
+
+    if (leftCol || rightCol)
+      return (
+        <Container fluid={true} style={{ padding: "1rem" }}>
+          <div className="d-block d-md-none">
+            {/*## When the window is smaller, mobile for example ##*/}
+            <Tabs
+              className="justify-content-center"
+              defaultActiveKey="Middle"
+              id="uncontrolled-tab-example"
+            >
+              {leftSize ? (
+                <Tab eventKey="Left" title="<____">
+                  {leftCol}
+                </Tab>
+              ) : null}
+              <Tab eventKey="Middle" title="__________">
+                {middleCol}
+              </Tab>
+              {rightSize ? (
+                <Tab eventKey="Right" title="____>">
+                  {rightCol}
+                </Tab>
+              ) : null}
+            </Tabs>
+          </div>
+          <div className="d-none d-md-block">
+            {/*## When the window is larger ##*/}
+            <Row>
+              {leftCol}
+              {middleCol}
+              {rightCol}
+            </Row>
+          </div>
+        </Container>
+      );
+    else return middleCol; //## When there's only a sinlge column ##
   }
 }
 
--- visitor-default-view/src/components/compColRenderer.jsx
+++ visitor-default-view/src/components/compColRenderer.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { Card, Row } from "react-bootstrap";
 import ComponentRenderer from "./componentRenderer";
-import { complementColor } from "./generalFunctions";
 
 const CompColRenderer = props => {
-  const { followstyle, backgroundColor } = props.style;
-  let thisStyle;
-  if (followstyle) {
-    thisStyle = {
-      backgroundColor: complementColor(backgroundColor)
-    };
-  }
   return (
-    <Card style={{ ...thisStyle, padding: "1%" }}>
-      <Card.Body>
+    <Card style={props.style}>
+      <Card.Body {...props}>
         {props.ids.map(id => {
           return (
-            <Row key={id} style={{ margin: "1%" }}>
-              <ComponentRenderer key={id} id={id}></ComponentRenderer>
+            <Row key={id}>
+              <ComponentRenderer key={id} id={id} />
             </Row>
           );
         })}
