f773e491bdbde72edd8b0d94d69725522c95674d
aaron <aaronmunstersbxl@gmail.com>
2019-12-18 19:53:03 +0100

    Removed circle, added unsuported backend support

--- visitor-default-view/src/components/woxComponents/clickablePictureRenderer.jsx
+++ visitor-default-view/src/components/woxComponents/clickablePictureRenderer.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import { getApiObject } from "./../generalFunctions";
 
+const fakePicture = {
+  caption: "Mountains",
+  src: "https://picsum.photos/id/1015/6000/4000",
+  small: "https://picsum.photos/id/1015/600/400",
+  width: 6,
+  height: 4,
+  location: [41.505, -0.09]
+};
+
 class CliclablePictureRenderer extends Component {
   state = {
     caption: null,
@@ -12,7 +21,8 @@ class CliclablePictureRenderer extends Component {
   };
 
   updatePicture = async () => {
-    const image = getApiObject("image", this.props.id);
+    let image = await getApiObject("image", this.props.id);
+    if (!image) image = fakePicture; // get's removed when pictures can be inserted
     this.setState({ ...image });
   };
 
@@ -25,7 +35,7 @@ class CliclablePictureRenderer extends Component {
     if (!src) return null; // picture not loaded yet
     return (
       <a href={this.props.content.link}>
-        <img style={{ borderRadius: "50%" }} src={src} alt={caption}></img>
+        <img src={src} alt={caption}></img>
       </a>
     );
   }
