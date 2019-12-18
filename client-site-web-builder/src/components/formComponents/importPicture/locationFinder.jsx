02eb55f189484ce4894738122d45da044991e1a2
aaron <aaronmunstersbxl@gmail.com>
2019-12-19 00:44:02 +0100

    Added basic support for querying locations

--- client-site-web-builder/src/components/formComponents/importPicture/locationFinder.jsx
+++ client-site-web-builder/src/components/formComponents/importPicture/locationFinder.jsx
@@ -1,1 +1,19 @@
-/*<Location onLocationFound={coords => console.log("found coords:", coords)} /> */
+import React, { Component } from "react";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "leaflet-control-geocoder";
+
+const Location = () => {
+  const geocoder = L.Control.Geocoder.nominatim();
+
+  const searchString = "VUB Brussel"; // Known to work
+
+  console.log(
+    geocoder.geocode(searchString, results => {
+      console.log(results);
+    })
+  );
+  return null;
+};
+
+export default Location;
