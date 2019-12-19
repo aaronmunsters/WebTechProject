19c67875342f41565c72c8bb7d16984a1faa9dbc
FastFoodChicken <aaronmunstersbxl@gmail.com>
2019-12-19 11:34:28 +0100

    Changed to functional component

--- client-site-web-builder/src/components/formComponents/importPicture/locationFinder.jsx
+++ client-site-web-builder/src/components/formComponents/importPicture/locationFinder.jsx
@@ -7,8 +7,8 @@ import "leaflet-control-geocoder";
 // For each of these location retrievers, global geocoder
 const geocoder = L.Control.Geocoder.nominatim();
 
-class Location extends Component {
-  getAnswers = inputValue => {
+const Location = () => {
+  const getAnswers = inputValue => {
     return new Promise((resolve, reject) => {
       geocoder.geocode(inputValue, results => {
         // The required attribute for the results is a label which is displayed as search options
@@ -17,17 +17,14 @@ class Location extends Component {
     });
   };
 
-  loadLocations = async (inputValue, callback) =>
-    callback(await this.getAnswers(inputValue));
-  render() {
-    return (
-      <AsyncSelect
-        cacheOptions
-        loadOptions={this.loadLocations}
-        defaultOptions
-      />
-    );
-  }
-}
+  return (
+    <AsyncSelect
+      cacheOptions
+      // cb is callback which is applied to results once they're loaded
+      loadOptions={async (input, cb) => cb(await getAnswers(input))}
+      defaultOptions
+    />
+  );
+};
 
 export default Location;
