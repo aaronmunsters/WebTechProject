fb66f7b7edb6a5a57755e143a2639ccd3506061d
wulfdewolf <wolf.de.wulf@vub.be>
2019-12-23 19:28:48 +0100

    fixed the hardcoded database

--- database/WoxDB.sql
+++ database/WoxDB.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.18, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
 --
 -- Host: localhost    Database: WoxDB
 -- ------------------------------------------------------
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `Comments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Comments` (
   `id` varchar(45) NOT NULL,
   `author` varchar(45) DEFAULT NULL,
@@ -48,7 +48,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `Config`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Config` (
   `id` varchar(20) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
@@ -73,7 +73,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `Images`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Images` (
   `id` varchar(45) NOT NULL,
   `extension` varchar(10) DEFAULT NULL,
@@ -94,6 +94,7 @@ CREATE TABLE `Images` (
 
 LOCK TABLES `Images` WRITE;
 /*!40000 ALTER TABLE `Images` DISABLE KEYS */;
+INSERT INTO `Images` VALUES ('6a9a6930-2505-11ea-b3ad-91a6032db6d2','.jpg','party','http://localhost:3001/v1/api/images/6a9a6930-2505-11ea-b3ad-91a6032db6d2.jpg',4,51,6000,4000,'http://localhost:3001/v1/api/compressed_images/6a9a6930-2505-11ea-b3ad-91a6032db6d2.jpg'),('7c06ca40-2507-11ea-b3ad-91a6032db6d2','.png','RedButton','http://localhost:3001/v1/api/images/7c06ca40-2507-11ea-b3ad-91a6032db6d2.png',NULL,NULL,200,200,'http://localhost:3001/v1/api/compressed_images/7c06ca40-2507-11ea-b3ad-91a6032db6d2.png'),('7cc1b2a0-2503-11ea-b3ad-91a6032db6d2','.jpg','Carousel1','http://localhost:3001/v1/api/images/7cc1b2a0-2503-11ea-b3ad-91a6032db6d2.jpg',NULL,NULL,1920,1080,'http://localhost:3001/v1/api/compressed_images/7cc1b2a0-2503-11ea-b3ad-91a6032db6d2.jpg'),('7ccf3540-2505-11ea-b3ad-91a6032db6d2','.jpg','friends','http://localhost:3001/v1/api/images/7ccf3540-2505-11ea-b3ad-91a6032db6d2.jpg',87,28,5472,3648,'http://localhost:3001/v1/api/compressed_images/7ccf3540-2505-11ea-b3ad-91a6032db6d2.jpg'),('83c2c210-2503-11ea-b3ad-91a6032db6d2','.JPG','Carousel2','http://localhost:3001/v1/api/images/83c2c210-2503-11ea-b3ad-91a6032db6d2.JPG',NULL,NULL,3940,2626,'http://localhost:3001/v1/api/compressed_images/83c2c210-2503-11ea-b3ad-91a6032db6d2.JPG'),('9ed7e810-2507-11ea-b3ad-91a6032db6d2','.png','Purple button','http://localhost:3001/v1/api/images/9ed7e810-2507-11ea-b3ad-91a6032db6d2.png',NULL,NULL,200,200,'http://localhost:3001/v1/api/compressed_images/9ed7e810-2507-11ea-b3ad-91a6032db6d2.png'),('a05e24d0-2505-11ea-b3ad-91a6032db6d2','.jpg','talking','http://localhost:3001/v1/api/images/a05e24d0-2505-11ea-b3ad-91a6032db6d2.jpg',4,51,5472,3648,'http://localhost:3001/v1/api/compressed_images/a05e24d0-2505-11ea-b3ad-91a6032db6d2.jpg');
 /*!40000 ALTER TABLE `Images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,7 +104,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `Layouts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Layouts` (
   `id` varchar(45) NOT NULL,
   `title` varchar(45) DEFAULT NULL,
@@ -130,7 +131,7 @@ CREATE TABLE `Layouts` (
 
 LOCK TABLES `Layouts` WRITE;
 /*!40000 ALTER TABLE `Layouts` DISABLE KEYS */;
-INSERT INTO `Layouts` VALUES ('Default','Default layout','single','rgb(50 , 50, 50)',0,'Woxpace','[]',0,'\"\"','[]','Default layout','2019-12-17','admin', 0, '/');
+INSERT INTO `Layouts` VALUES ('Default','Default layout','small-left','rgb(255, 152, 0)',1,'Woxpace','[\"Default\", \"b436d250-2504-11ea-b3ad-91a6032db6d2\"]',1,'WoxPace™ Made by Corneel - Aaron - Wolf','[\"Default\", \"b436d250-2504-11ea-b3ad-91a6032db6d2\"]','This is the default layout that will be used when no other layout is specified','2019-12-22','admin',0,'/');
 /*!40000 ALTER TABLE `Layouts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,7 +141,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `Pages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Pages` (
   `id` varchar(45) NOT NULL,
   `title` varchar(45) DEFAULT NULL,
@@ -163,7 +164,7 @@ CREATE TABLE `Pages` (
 
 LOCK TABLES `Pages` WRITE;
 /*!40000 ALTER TABLE `Pages` DISABLE KEYS */;
-INSERT INTO `Pages` VALUES ('Default','Default page','admin',1,'[]','[]','[]','2019-12-07','default','Default','default page');
+INSERT INTO `Pages` VALUES ('b436d250-2504-11ea-b3ad-91a6032db6d2','Pictures Page','admin',1,'[\"060f6d80-2505-11ea-b3ad-91a6032db6d2\"]','[\"a15e56c0-2505-11ea-b3ad-91a6032db6d2\"]','[]','2019-12-22','pictures','Default','Show off the picturefolder and'),('Default','Homepage','admin',1,'[\"4180c870-2503-11ea-b3ad-91a6032db6d2\"]','[\"d0378310-2503-11ea-b3ad-91a6032db6d2\", \"bf0e5dd0-2507-11ea-b3ad-91a6032db6d2\"]','[]','2019-12-22','default','Default','default page');
 /*!40000 ALTER TABLE `Pages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -173,7 +174,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `Users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Users` (
   `id` varchar(45) NOT NULL,
   `email` varchar(45) DEFAULT NULL,
@@ -201,7 +202,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `WoxComponents`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `WoxComponents` (
   `id` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `editor` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -224,12 +225,9 @@ CREATE TABLE `WoxComponents` (
 
 LOCK TABLES `WoxComponents` WRITE;
 /*!40000 ALTER TABLE `WoxComponents` DISABLE KEYS */;
+INSERT INTO `WoxComponents` VALUES ('060f6d80-2505-11ea-b3ad-91a6032db6d2','admin','Pictures Page Heading',NULL,'text','{\"text\": \"# YOU MADE IT!\\nHere is a nice example of a picture folder woxComponent, as you can see it\'s perfect \\nfor showing pictures to friends and family\"}','[\"b436d250-2504-11ea-b3ad-91a6032db6d2\"]','2019-12-23','The heading of the pictures page',0,'[]'),('4180c870-2503-11ea-b3ad-91a6032db6d2','admin','Welcome Text',NULL,'text','{\"text\": \"# Welcome to WoxPace\\nWe are very glad to see you here. This component is called a Text component, \\nit can be edited by an admin by going to the *woxComponents* tab and editing *Welcome Text*.\"}','[\"Default\"]','2019-12-23','The welcome text for new admins of the website',0,'[]'),('859a9280-2507-11ea-b3ad-91a6032db6d2','admin','Example ClickableButton Red',NULL,'clickablePicture','{\"id\": \"7c06ca40-2507-11ea-b3ad-91a6032db6d2\", \"link\": \"https://tinyurl.com/uvcng7v\"}','[]','2019-12-22','The red example clickable button',0,'[]'),('a15e56c0-2505-11ea-b3ad-91a6032db6d2','admin','Example PictureFolder',NULL,'pictureFolder','{\"ids\": [\"6a9a6930-2505-11ea-b3ad-91a6032db6d2\", \"7ccf3540-2505-11ea-b3ad-91a6032db6d2\", \"a05e24d0-2505-11ea-b3ad-91a6032db6d2\"], \"locationActive\": true}','[\"b436d250-2504-11ea-b3ad-91a6032db6d2\"]','2019-12-22','All these pictures where free to use from unsplash',0,'[]'),('a5d17d70-2507-11ea-b3ad-91a6032db6d2','admin','Example ClickableButton Purple',NULL,'clickablePicture','{\"id\": \"9ed7e810-2507-11ea-b3ad-91a6032db6d2\", \"link\": \"https://tinyurl.com/uvcng7v\"}','[]','2019-12-22','the purple clickable button',0,'[]'),('bf0e5dd0-2507-11ea-b3ad-91a6032db6d2','admin','Example Container',NULL,'container','{\"ids\": [\"859a9280-2507-11ea-b3ad-91a6032db6d2\", \"a5d17d70-2507-11ea-b3ad-91a6032db6d2\"]}','[\"Default\"]','2019-12-22','An example container which holds 2 clickable buttons',0,'[]'),('d0378310-2503-11ea-b3ad-91a6032db6d2','admin','Example Carousel',NULL,'carousel','{\"ids\": [\"7cc1b2a0-2503-11ea-b3ad-91a6032db6d2\", \"83c2c210-2503-11ea-b3ad-91a6032db6d2\"], \"captions\": false, \"captionActive\": false, \"customCaption\": false, \"singleCaption\": false}','[\"Default\"]','2019-12-22','This is an example carousel woxComponent, it contains 2 beautiful pictures of carousels',0,'[]');
 /*!40000 ALTER TABLE `WoxComponents` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'WoxDB'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -240,4 +238,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-19  1:11:39
+-- Dump completed on 2019-12-23 19:23:13
