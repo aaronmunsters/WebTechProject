40be51405d0488d75d54e5c6b93c135a57712288
wulfdewolf <wolf.de.wulf@vub.be>
2019-12-23 19:31:37 +0100

    remove the production builds from the gitignores

---
+++ rendering-engine/build/precache-manifest.b60a159a35a3f85654f3a7e2a06703c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de28b10df170591b052bafca7a817b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa99a59807886e145eea",
+    "url": "/static/css/2.7717d4a8.chunk.css"
+  },
+  {
+    "revision": "a215f9974e1b6c1955af",
+    "url": "/static/css/main.6de2710f.chunk.css"
+  },
+  {
+    "revision": "aa99a59807886e145eea",
+    "url": "/static/js/2.a603de98.chunk.js"
+  },
+  {
+    "revision": "a215f9974e1b6c1955af",
+    "url": "/static/js/main.9daed87f.chunk.js"
+  },
+  {
+    "revision": "47d8ba0457ad3dfd189a",
+    "url": "/static/js/runtime-main.54141533.js"
+  }
+]);
---
+++ web-builder/build/precache-manifest.807c547e5d3baf8f0b740c07a6d6bfe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba50e55cb91aa4c4b863a63c0ec2a831",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf650e584c8815e47ff1",
+    "url": "/static/css/2.946b883b.chunk.css"
+  },
+  {
+    "revision": "10f7551fe84257f8f367",
+    "url": "/static/css/main.b17fe23d.chunk.css"
+  },
+  {
+    "revision": "cf650e584c8815e47ff1",
+    "url": "/static/js/2.3311f566.chunk.js"
+  },
+  {
+    "revision": "10f7551fe84257f8f367",
+    "url": "/static/js/main.f8e4f115.chunk.js"
+  },
+  {
+    "revision": "5f3f474c4d93622dea15",
+    "url": "/static/js/runtime-main.c92f0691.js"
+  }
+]);
